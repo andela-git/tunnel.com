74cf3c32c0db1b733917430e3198e397dcf70052
Wale Ayandiran <waleayandiran@Wales-MacBook-Pro.local>
2018-07-24 13:33:20 +0100

    set up develop branch server, with babel and nodemon to dev, build and serve npm run

---
+++ server/app.js
@@ -0,0 +1,36 @@
+/**
+ * @app.js Application's app root file for express, morgan, body-parser & default route setup
+ * @author Wale Ayandiran
+ * @requires NPM:express
+ * @requires NPM:morgan
+ * @requires NPM:express
+ * @requires NPM:path
+ */
+
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+// load routes into the application
+// import routes from './src/router/routes';
+
+// set up expres app
+const app = express();
+
+// Log request to the console using morgan
+app.use(logger('combined'));
+
+// parse incoming requests data as json & respond as json using bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// routes(app);
+
+// setup a default cathch all route for lost/wrong api endpoints
+app.get('*', (req, res) => res.status(200).send({
+  status: 200,
+  data: {
+    message: 'alice in wonderland',
+  },
+}));
+
+module.exports = app;
---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+/**
+ * @./src/index.js Http Server point setup
+ * @author Wale Ayandiran
+ * @requires NPM:http
+ * @requires app
+ */
+
+import http from 'http';
+import app from '../app';
+
+// setup port ton listen from env port if its set else default to port 8000
+const port = parseInt(process.env.PORT, 10) || 7000;
+app.set('port', port);
+
+// create actual actual http server and listen for connections
+const server = http.createServer(app);
+server.listen(port);
+
+console.log(`server listening on port: ${port}`);
