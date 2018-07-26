4b96ccfb580cccbb7c55caa62100917654261e93
Wale Ayandiran <waleayandiran@Wales-MacBook-Pro.local>
2018-07-26 11:18:00 +0100

    setup test structure

---
+++ test/user/user.Spec.js
@@ -0,0 +1,23 @@
+/**
+ * Testing file for Users route
+ */
+
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+
+chai.use(chaiHttp);
+
+// playground with test datas
+// database for tests, migration
+
+// actual call to main route/function
+
+// assert the calls passes
+
+describe('User Endpoints', () => {
+  describe('account signup', () => {
+    it('email must be valid & not empty', () => {
+      chai.assert.equal();
+    });
+  });
+});
