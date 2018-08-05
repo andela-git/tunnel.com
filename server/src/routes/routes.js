f73ff23899a0844392c21f57e0dd7c23734d509b
wale ayandiran <waleayandiran@wales-MacBook-Pro.local>
2018-08-05 23:48:44 +0100

    controller users,  token middleware routes file

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,29 +1,36 @@
-/**
- * @author Wale ayandiran
- * routes file for all endpoints
- */
-import express from 'express';
+// /**
+//  * @author Wale ayandiran
+//  * routes file for all endpoints
+//  */
+// import express from 'express';
 
-import UserController from '../controllers/users';
-import PostController from '../controllers/posts';
-import verifyToken from '../middlewares/Token';
+// import UserController from '../controllers/users';
+// import PostController from '../controllers/posts';
+// import verifyToken from '../middlewares/Token';
 
-const app = express();
-// GET route for aimless /api routes
-app.get('/api/', (req, res) => res.status(200).send({
-  message: 'successful',
-  data: 'Api in version 1.0.0 currently, the endpoints are available in the format /api/v1/ ',
-}));
+// const app = express();
+// const router = express.Router();
 
-/**
-   * Routes for Users
-   */
-app.post('/api/v1/user/new', UserController.create);
-app.post('/api/v1/user/login', UserController.login);
+// app.use('/api', router);
 
-/**
-   * Post/articles routes
-   */
-// app.post('/api/v1/post/new', verifyToken, PostController.create);
+// // GET route for aimless /api routes
+// router.get('/api', (req, res) => res.status(200).send({
+//   message: 'successful',
+//   data: 'Api in version 1.0.0 currently, the endpoints are available in the format /api/v1/ ',
+// }));
 
-export default app;
+// /**
+//    * Routes for Users
+//    */
+// router.post('/v1/user/new', UserController.create);
+// router.post('/v1/user/login', UserController.login);
+
+// // DemoToken for test purposes only
+// router.get('/getToken', UserController.getToken);
+
+// /**
+//    * Post/articles routes
+//    */
+// router.post('/v1/post/new', verifyToken, PostController.create);
+
+// export default router;
